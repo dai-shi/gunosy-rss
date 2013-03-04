aaa573e8417f1cd442daa06e6df4695ae9043103
daishi <daishi@axlight.com>
2013-03-05 00:02:35 +0900

    first working version

--- app.js
+++ app.js
@@ -25,11 +25,68 @@
 /*jshint es5:true */
 
 var express = require('express');
+var request = require('request');
+var htmlparser = require('htmlparser');
+var jsonpath = require('JSONPath').eval;
+var rss = require('rss');
+var S = require('string');
 
 var app = express();
+app.use(express.logger());
+
+function generate_rss(req, gunosy_id, callback) {
+  request({
+    url: 'http://gunosy.com/' + gunosy_id,
+    headers: {
+      'User-Agent': 'Mozilla/5.0'
+    }
+  }, function(err, res, body) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    var handler = new htmlparser.DefaultHandler(function(err, dom) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      var entries = jsonpath(dom, '$..children[?(@.type=="tag" && @.name=="div" && @.attribs.class=="entry-content")]');
+      var feed = new rss({
+        title: 'Gunosy Summary of ' + gunosy_id,
+        feed_url: req.url,
+        site_url: 'http://gunosy.com/' + gunosy_id
+      });
+      entries.forEach(function(entry) {
+        var entry_title = jsonpath(entry, '$..children[?(@.type=="tag" && @.name=="h1" && @.attribs.class=="entry-title")]');
+        var entry_summary = jsonpath(entry, '$..children[?(@.type=="tag" && @.name=="div" && @.attribs.class=="entry-summary")]');
+        feed.item({
+          title: jsonpath(entry_title, '$..children[?(@.type=="text")].data'),
+          url: S(jsonpath(entry_title, '$..children[?(@.type=="tag" && @.name=="a")].attribs.href')).decodeHTMLEntities().s,
+          description: jsonpath(entry_summary, '$..children[?(@.type=="text")].data')
+        });
+      });
+      callback(null, feed.xml());
+    });
+    var parser = new htmlparser.Parser(handler);
+    parser.parseComplete(body);
+  });
+}
 
 app.get('/', function(req, res) {
-    res.send('It works!');
+  res.send('It works!');
 });
+
+app.get(new RegExp('^/(.+)\.rss$'), function(req, res) {
+  var gunosy_id = req.params[0];
+  generate_rss(req, gunosy_id, function(err, result) {
+    if (err) {
+      console.log('failed in generate_rss', err);
+      res.send(500, 'failed generating rss');
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 
 app.listen(process.env.PORT || 5000);
